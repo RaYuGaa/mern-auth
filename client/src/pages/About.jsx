0c260b16638c87077a6d782d1f861b2296f81954
Rayuga Nub <75535740+RaYuGaa@users.noreply.github.com>
2024-06-27 11:31:09 +0545

    complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
-    </div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+    <h1 className='text3xl font-bold mb-4 text-slate-800'>About</h1>
+    <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere impedit officiis molestias, unde eveniet illum, ut cupiditate, neque quisquam voluptate similique totam delectus eligendi et? Adipisci, sunt? Tenetur, excepturi et.</p>
+    <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere impedit officiis molestias, unde eveniet illum, ut cupiditate, neque quisquam voluptate similique totam delectus eligendi et? Adipisci, sunt? Tenetur, excepturi et.</p>
+    <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere impedit officiis molestias, unde eveniet illum, ut cupiditate, neque quisquam voluptate similique totam delectus eligendi et? Adipisci, sunt? Tenetur, excepturi et.</p>
+  </div>
   )
 }
